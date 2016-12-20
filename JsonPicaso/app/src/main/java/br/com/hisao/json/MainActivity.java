4d85098f27f9538a1a5e3f5c07f934c491df7db4
CIPUCRS\Aluno <gremmel@gmail.com>
2016-12-20 21:09:19 -0200

    commit volley shiting usage

--- JsonPicaso/app/src/main/java/br/com/hisao/json/MainActivity.java
+++ JsonPicaso/app/src/main/java/br/com/hisao/json/MainActivity.java
@@ -8,6 +8,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -24,35 +31,78 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                AsyncTask<Void, Void, JSONArray> taskGetJson = new AsyncTask<Void, Void, JSONArray>() {
-                    @Override
-                    protected JSONArray doInBackground(Void... params) {
-                        return JsonUtil.getJson("https://api.github.com/users/gustavoemmel/repos");
-                    }
 
-                    @Override
-                    protected void onPostExecute(JSONArray jsonArray) {
-                        super.onPostExecute(jsonArray);
-                        if (BuildConfig.DEBUG){
-                            try {
-                                for (int i = 0; i < jsonArray.length(); i++) {
-                                    JSONObject explrObject;
-                                    explrObject = jsonArray.getJSONObject(i);
-                                    Log.d("Gustavo", explrObject.getString("name"));
-                                    Log.d("Gustavo", explrObject.getJSONObject("owner").getString("avatar_url"));
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                String url ="https://api.github.com/users/gustavoemmel/repos";
+
+                // Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                //mTextView.setText("Response is: "+ response.substring(0,500));
+
+                                try {
+                                    JSONArray jsonArray = new JSONArray(response);
+
+                                    ListView lstNumbers =(ListView)findViewById(R.id.lsv_users);
+                                    lstNumbers.setAdapter(new GithubUsersAdapter(jsonArray, MainActivity.this));
+
+                                    for (int i=0; i<= jsonArray.length(); i++){
+
+                                        JSONObject explrObject;
+                                        explrObject = jsonArray.getJSONObject(i);
+                                        Log.d("Gustavo", explrObject.getString("name"));
+                                        Log.d("Gustavo", explrObject.getJSONObject("owner").getString("avatar_url"));
+                                    }
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
                                 }
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            } catch (Exception e){
-                                e.printStackTrace();
-                            }
-                        }
 
-                        ListView lstNumbers =(ListView)findViewById(R.id.lsv_users);
-                        lstNumbers.setAdapter(new GithubUsersAdapter(jsonArray, MainActivity.this));
+                                Log.d("Gustavo", "Response is: "+ response.substring(0,500));
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //mTextView.setText("That didn't work!");
                     }
-                };
-                taskGetJson.execute(null, null, null);
+                });
+// Add the request to the RequestQueue.
+                queue.add(stringRequest);
+
+//
+//                AsyncTask<Void, Void, JSONArray> taskGetJson = new AsyncTask<Void, Void, JSONArray>() {
+//                    @Override
+//                    protected JSONArray doInBackground(Void... params) {
+//                        return JsonUtil.getJson("https://api.github.com/users/gustavoemmel/repos");
+//                    }
+//
+//                    @Override
+//                    protected void onPostExecute(JSONArray jsonArray) {
+//                        super.onPostExecute(jsonArray);
+//                        if (BuildConfig.DEBUG){
+//                            try {
+//                                for (int i = 0; i < jsonArray.length(); i++) {
+//                                    JSONObject explrObject;
+//                                    explrObject = jsonArray.getJSONObject(i);
+//                                    Log.d("Gustavo", explrObject.getString("name"));
+//                                    Log.d("Gustavo", explrObject.getJSONObject("owner").getString("avatar_url"));
+//                                }
+//                            } catch (JSONException e) {
+//                                e.printStackTrace();
+//                            } catch (Exception e){
+//                                e.printStackTrace();
+//                            }
+//                        }
+//
+//                        ListView lstNumbers =(ListView)findViewById(R.id.lsv_users);
+//                        lstNumbers.setAdapter(new GithubUsersAdapter(jsonArray, MainActivity.this));
+//                    }
+//                };
+//                taskGetJson.execute(null, null, null);
             }
     }
 
